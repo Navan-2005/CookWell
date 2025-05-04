60513b245fe1c9835fba20db2c743b7786908bd2
Navan <navan05please@gmail.com>
2025-05-04 13:52:39 +0530

    Basic Backend

--- Backend/server.js
+++ Backend/server.js
@@ -3,11 +3,15 @@ const express=require('express');
 const cors=require('cors');
 const app=express();
 const aiRoutes=require('./routes/airoutes')
+const userRoutes=require('./routes/userRoutes')
+const connectDB=require('./db/db')
+connectDB();
 
 const port=3000;
 app.use(cors());
 app.use(express.json());
 
 app.use('/ai',aiRoutes);
+app.use('/user',userRoutes);
 
 app.listen(port,()=>console.log(`Server running on port ${port}`));
