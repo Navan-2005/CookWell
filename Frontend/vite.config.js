e3ed0b84e7d874f17f68f3fc46d22f4eccd1abd0
Navan <navan05please@gmail.com>
2025-08-02 23:56:09 +0530

    Dockerised

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,8 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: '192.168.0.197',  // binds to all network interfaces
-    port: 5173        // optional; default Vite port
-  }
 })
